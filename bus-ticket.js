435aca7d642e2f4ecc584e6f18c222c55ceb88ea
Alal Hosen <alalhosen1992620120@gmail.com>
2025-06-05 00:27:52 +0600

    count function

--- bus-ticket.js
+++ bus-ticket.js
@@ -1,20 +1,14 @@
 function play() {
-    const bannerSection = document.getElementById('banner-section');
-    bannerSection.classList.add('hidden');
+  const bannerSection = document.getElementById("banner-section");
+  bannerSection.classList.add("hidden");
 
-    const bestSection = document.getElementById('best-section');
-    bestSection.classList.add('hidden');
+  const bestSection = document.getElementById("best-section");
+  bestSection.classList.add("hidden");
 
-    // const ticketSection = document.getElementById('ticket-section');
-    // ticketSection.classList.replace('ticket-section')
-    // console.log(ticketSection)
+  // const ticketSection = document.getElementById('ticket-section');
+  // ticketSection.classList.replace('ticket-section')
+  // console.log(ticketSection)
 }
-
-
-
-
-
-
 
 // const allBtn = document.getElementsByClassName("btn-icon");
 // let count = 0;
@@ -32,7 +26,6 @@ function play() {
 
 // select ticket seat//
 
-
 // function handleClick (event) {
 //     console.log(event.target.parentNode.parentNode.childNodes[5].innerText);
 // }
@@ -41,8 +34,13 @@ const allDiv = document.getElementsByClassName("seat-btn-icon");
 let count = 0;
 
 for (const div of allDiv) {
-  div.addEventListener("click", function(){
+  div.addEventListener("click", function () {
     count = count + 1;
-    console.log(count)
-  })
+    setInnerText("seat-zero", count)
+  });
+}
+
+
+function setInnerText(id, value) {
+document.getElementById(id).innerText = value;
 }
