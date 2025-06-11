7e42f0dfde6296e71f79e08b9fc55e81391115c6
Alal Hosen <alalhosen1992620120@gmail.com>
2025-06-11 23:42:39 +0600

    function totalCost

--- bus-ticket.js
+++ bus-ticket.js
@@ -74,13 +74,17 @@ totalCost("total-price", parseInt(seatPrice))
   });
 }
 
+
+function setInnerText(id, value) {
+  document.getElementById(id).innerText = value; 
+}
+
+
+
+
 function totalCost (id, value) {
   const totalCost = document.getElementById("total-price").innerText;
     const convertedTotalCost = parseInt(totalCost);
     const sum = convertedTotalCost + parseInt(seatPrice);
   setInnerText("total-price", sum);
-}
-
-function setInnerText(id, value) {
-  document.getElementById(id).innerText = value; 
 }
